90a59fb214b73ac12c26bd551bf49591d5a8ae8f
Theo Sementa <theosementa@gmail.com>
2025-02-25 07:05:30 +0100

    feat: add list of car

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,48 @@
-import { SafeAreaView, Text, View } from "react-native"
+import { observer } from "mobx-react"
+import { Button, SafeAreaView, SectionList, Text, View } from "react-native"
+import { CarStore } from "../src/stores/car.store"
 
-const HomeView = () => {
+const HomeView = observer(() => {
+  const carStore = CarStore.shared
+
+  const sections = Object.keys(carStore.carsByMakes).map((make) => ({
+    title: make,
+    data: carStore.carsByMakes[make]
+  }))
+
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1">
         <Text className="text-xl p-4">Hello World!</Text>
+
+        <Button title="Add new car" onPress={() => carStore.addNewCar()}></Button>
+
+        {
+          carStore.cars.map((car) => (
+            <Text>{ car.model }</Text>
+          ))
+        }
+
+        <SectionList
+          sections={sections}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <Text>{item.model}</Text>
+          )}
+          renderSectionHeader={({ section }) => (
+            <View>
+              <View className="h-6 bg-gray-100" />
+              <View className="bg-gray-100">
+                <Text className="text-xl font-bold">{section.title}</Text>
+              </View>
+            </View>
+          )}
+          contentContainerStyle={{ padding: 24 }}
+        >
+        </SectionList>
       </View>
     </SafeAreaView>
   )
-}
+})
 
 export default HomeView
--- src/stores/car.store.ts
+++ src/stores/car.store.ts
@@ -10,11 +10,29 @@ export class CarStore {
 
   cars: CarModel[] = [
     new CarModel(1, 'Ferrari', '812 Superfast', 2017),
-    new CarModel(2, 'Ferrari', '812 Superfast', 2017),
-    new CarModel(3, 'Ferrari', '812 Superfast', 2017),
-    new CarModel(4, 'Ferrari', '812 Superfast', 2017),
-    new CarModel(5, 'Ferrari', '812 Superfast', 2017),
-    new CarModel(6, 'Ferrari', '812 Superfast', 2017)
+    new CarModel(2, 'Ferrari', '488 Pista', 2019),
+    new CarModel(3, 'Audi', 'RS6', 2020),
+    new CarModel(4, 'Audi', 'RS3', 2018),
+    new CarModel(5, 'Mercedes', 'CLA45s', 2021)
   ]
+
+  carsByMakes = this.sortCarsByMake()
+
+  sortCarsByMake(): { [key: string]: CarModel[] } {
+    return this.cars.reduce((acc, car) => {
+
+      if (!acc[car.make]) {
+        acc[car.make] = []
+      }
+      acc[car.make].push(car);
+      return acc;
+    }, {} as { [key: string]: CarModel[] });
+  }
+
+  addNewCar() {
+    this.cars.push(
+      new CarModel(6, 'Mercedes', 'CLE53', 2024)
+    )
+  }
 
 }
